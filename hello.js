c671f1c0dd41054aeaa2a79f597865984a990a3f
Valjean <valjeanbud@gmail.com>
2015-08-11 00:16:33 -0700

    working locally but not on webtask server

--- hello.js
+++ hello.js
@@ -22,19 +22,25 @@ module.exports = function (ctx, cb) {
 
 	var request = Promise.promisify(requestOriginal.defaults({
 		headers: {
-			'User-Agent': ctx.data.githubUser,
-			'Authorization': 'token ' + ctx.data.githubToken
-		}
+			//'User-Agent': ctx.data.GITHUB_USER,
+			'User-Agent': 'vbud',
+			'Authorization': 'token ' + ctx.data.GITHUB_TOKEN
+		},
+		json: true
 	}));
 
+	var filename = 'CHANGELOG.md';
+	var branch = 'master';
 	var changelog = ''; //changelog
 	var currentCommit; //current commit
 
 	//retrieve the content of the blob object for CHANGELOG.md
 	//TODO: This assumes there is only one branch; it does not do the first two steps in the how-to block above. A production version of this would need to do that.
-	request({uri: 'https://api.github.com/repos/vbud/auth0-hacking-time/contents/CHANGELOG.md'})
+	request({
+		uri: 'https://api.github.com/repos/vbud/auth0-hacking-time/contents/' + filename + '?ref=' + branch
+	})
 		.then(function (results) {
-			var content = JSON.parse(results[1]).content;
+			var content = results[1].content;
 
 			var changelogByteArray = base64.toByteArray(content.replace('\n', ''));
 
@@ -43,28 +49,29 @@ module.exports = function (ctx, cb) {
 			});
 
 			//get the most recent commit message
-			return request({uri: 'https://api.github.com/repos/vbud/auth0-hacking-time/commits'});
+			//return request({uri: 'https://api.github.com/repos/vbud/auth0-hacking-time/commits'});
+
+			//get the reference to HEAD
+			return request({
+				uri: 'https://api.github.com/repos/vbud/auth0-hacking-time/git/refs/heads/' + branch
+			});
 		})
 		.then(function (results) {
-			var commits = JSON.parse(results[1]);
-			commits.sort(function (a, b) {
-				a = new Date(a.commit.author.date);
-				b = new Date(b.commit.author.date);
-				if (a > b) {
-					return 1;
-				} else if (a < b) {
-					return -1;
-				}
-				return 0;
+			var ref = results[1];
+
+			//get more information about the commit using its SHA
+			return request({
+				uri: 'https://api.github.com/repos/vbud/auth0-hacking-time/git/commits/' + ref.object.sha
 			});
-			currentCommit = commits[0];
-			changelog = '- ' + currentCommit.commit.message + '\n' + changelog;
+		})
+		.then(function (results) {
+			currentCommit = results[1];
+			changelog = '- ' + currentCommit.message + '\n' + changelog;
 
 			//post a blob with the new changelog
 			return request({
 				uri: 'https://api.github.com/repos/vbud/auth0-hacking-time/git/blobs',
 				method: 'POST',
-				json: true,
 				body: {
 					content: changelog,
 					encoding: 'utf-8'
@@ -72,9 +79,56 @@ module.exports = function (ctx, cb) {
 			});
 		})
 		.then(function (results) {
-			console.log(results[1])
+			var blob = results[1];
+
+			//create a tree for the new blob
+			return request({
+				uri: 'https://api.github.com/repos/vbud/auth0-hacking-time/git/trees',
+				method: 'POST',
+				body: {
+					base_tree: currentCommit.tree.sha,
+					tree: [{
+						'path': filename,
+						'mode': '100644',
+						'type': 'blob',
+						'sha': blob.sha
+					}]
+				}
+			});
+
+		})
+		.then(function (results) {
+			var tree = results[1];
+
+			//create a commit pointing to the new tree
+			return request({
+				uri: 'https://api.github.com/repos/vbud/auth0-hacking-time/git/commits',
+				method: 'POST',
+				body: {
+					message: 'WEBTASK: update changelog with most recent commit message',
+					parents: [currentCommit.sha],
+					tree: tree.sha
+				}
+			});
+		})
+		.then(function (results) {
+			var commit = results[1];
+
+			//update branch HEAD
+			return request({
+				uri: 'https://api.github.com/repos/vbud/auth0-hacking-time/git/refs/heads/' + branch,
+				method: 'PATCH',
+				body: {
+					sha: commit.sha
+				}
+			});
+
+		})
+		.then(function (results) {
+			console.log(results[1]);
 
 			cb(null, changelog);
+
 		})
 		.catch(function (err) {
 			console.error(err);
